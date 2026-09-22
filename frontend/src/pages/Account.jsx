5d7a45ffa18ea33f92b700c7a092443caec26707
Burak Bas <burakbas2004@gmail.com>
2026-09-22 23:45:23 +0300

    Modified Files

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,9 +1,11 @@
 // TODO: Update Account interface depending on roles (guide and lead-guide)
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import Error from './Error';
+import { updateMe, updateMyPassword } from '../services/userService';
+import ShowAlert from '../components/Alert';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -22,6 +24,55 @@ function NavItem({ link, text, icon, active = false }) {
 
 export default function Account() {
     const { user } = useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [photo, setPhoto] = useState(null);
+
+    const [passwordCurrent, setPasswordCurrent] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+    const [loading, setLoading] = useState(false);
+
+    const handleUserDataSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            setLoading(true);
+            const formData = new FormData();
+            formData.append('name', name);
+            formData.append('email', email);
+            if (photo) {
+                formData.append('photo', photo);
+            };
+
+            await updateMe(formData);
+            ShowAlert('success', 'Account settings updated successfully!');
+        } catch (err) {
+            ShowAlert('error', (err.response?.data?.message || 'Update Faailed!'))
+        } finally {
+            setLoading(false)
+        };
+    };
+
+    const handlePasswordSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            setLoading(true);
+            await updateMyPassword({ passwordCurrent, password, passwordConfirm });
+            ShowAlert('success', 'Password updated successfully!');
+            setPasswordCurrent('');
+            setPassword('');
+            setPasswordConfirm('');
+        } catch (err) {
+            ShowAlert('error', err.response?.data?.message || 'Password update failed!');
+        } finally {
+            setLoading(false);
+        };
+    };
+
     if (!user) return <Error message={`You are not logged in!`} />
 
     return (
@@ -51,18 +102,18 @@ export default function Account() {
                 <div className='user-view__content'>
                     <div className='user-view__form-container'>
                         <h2 className='heading-secondary ma-bt-md'>Your account settings</h2>
-                        <form className='form form-user-data'>
+                        <form className='form form-user-data' onSubmit={handleUserDataSubmit}>
                             <div className='form__group'>
                                 <label className='form__label' htmlFor='name'>Name</label>
-                                <input id='name' className='form__input' type='text' defaultValue={user.name} required name='name' />
+                                <input id='name' className='form__input' type='text' defaultValue={name} onChange={e => setName(e.target.value)} required name='name' />
                             </div>
                             <div className='form__group ma-bt-md'>
                                 <label className='form__label' htmlFor='email'>Email</label>
-                                <input id='email' className='form__input' type='email' defaultValue={user.email} required name='email' />
+                                <input id='email' className='form__input' type='email' defaultValue={email} onChange={e => setEmail(e.target.value)} required name='email' />
                             </div>
                             <div className='form__group form__photo-upload'>
                                 <img className='form__user-photo' src={`${BACKEND_URL}/img/users/${user.photo}`} alt={`${user.name}'s photo`} />
-                                <input className='form__upload' type='file' accept='image/*' id='photo' name='photo'  />
+                                <input className='form__upload' type='file' accept='image/*' id='photo' onChange={e => setPhoto(e.target.files[0])} name='photo'  />
                                 <label className='form__label' htmlFor='photo'>Choose new photo</label>
                             </div>
                             <div className='form__group right'>
@@ -75,18 +126,18 @@ export default function Account() {
                     </div>
                     <div className='user-view__form-container'>
                         <h2 className='heading-secondary ma-bt-md'>Password change</h2>
-                        <form className='form form-user-password'>
+                        <form className='form form-user-password' onSubmit={handlePasswordSubmit}>
                             <div className='form__group'>
                                 <label className='form__label' htmlFor='password-current'>Current password</label>
-                                <input id='password-current' className='form__input' type='password' placeholder='••••••••' minLength={8} required />
+                                <input id='password-current' className='form__input' type='password' placeholder='••••••••' value={passwordCurrent} onChange={e => setPasswordCurrent(e.target.value)} minLength={8} required />
                             </div>
                             <div className='form__group'>
                                 <label className='form__label' htmlFor='password'>New password</label>
-                                <input id='password' className='form__input' type='password' placeholder='••••••••' minLength={8} required />
+                                <input id='password' className='form__input' type='password' placeholder='••••••••' value={password} onChange={e => setPassword(e.target.value)} minLength={8} required />
                             </div>
                             <div className='form__group ma-bt-lg'>
                                 <label className='form__label' htmlFor='password-confirm'>Confirm password</label>
-                                <input id='password-confirm' className='form__input' type='password' placeholder='••••••••' minLength={8} required />
+                                <input id='password-confirm' className='form__input' type='password' placeholder='••••••••' value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} minLength={8} required />
                             </div>
                             <div className='form__group right'>
                                 <button type='submit' className='btn btn--small btn--green btn--save-password'>Save password</button>
